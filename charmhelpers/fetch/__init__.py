88e8f83e8a6f6f75cf5b4ce190016ea6312c1922
Johan Hallbäck <johan.hallback@scania.com>
2019-06-05 14:40:53 +0200

    Fixes for yum operations in fetch module
    - prevents CentOS deployments from using Ubuntu specific functions in
      fetch/__init__.py
    - removed dependency on yum.py from Python2 in fetch/centos.py

--- charmhelpers/fetch/__init__.py
+++ charmhelpers/fetch/__init__.py
@@ -83,12 +83,15 @@ __platform__ = get_platform()
 module = "charmhelpers.fetch.%s" % __platform__
 fetch = importlib.import_module(module)
 
+# This is the one breaking our stuff with yum for Python 2.x
 filter_installed_packages = fetch.filter_installed_packages
-filter_missing_packages = fetch.filter_missing_packages
-install = fetch.apt_install
-upgrade = fetch.apt_upgrade
-update = _fetch_update = fetch.apt_update
-purge = fetch.apt_purge
+# Does not exist in centos.py, move to ubuntu specific
+#filter_missing_packages = fetch.filter_missing_packages
+# These four moved to platform specific
+#install = fetch.apt_install
+#upgrade = fetch.apt_upgrade
+#update = _fetch_update = fetch.apt_update
+#purge = fetch.apt_purge
 add_source = fetch.add_source
 
 if __platform__ == "ubuntu":
@@ -103,8 +106,19 @@ if __platform__ == "ubuntu":
     apt_unhold = fetch.apt_unhold
     import_key = fetch.import_key
     get_upstream_version = fetch.get_upstream_version
+    # new adds
+    filter_missing_packages = fetch.filter_missing_packages
+    install = fetch.apt_install
+    upgrade = fetch.apt_upgrade
+    update = _fetch_update = fetch.apt_update
+    purge = fetch.apt_purge
 elif __platform__ == "centos":
     yum_search = fetch.yum_search
+    # new adds
+    install = fetch.install
+    upgrade = fetch.upgrade
+    update = _fetch_update = fetch.update
+    purge = fetch.purge
 
 
 def configure_sources(update=False,
